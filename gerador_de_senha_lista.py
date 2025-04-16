330fbe4a85c34b9d105070f3897047631c2f4426
Devhiago <dev.hiagoalves@gmail.com>
2025-04-16 20:29:14 -0300

    melhora a listagem de senhas e adiciona tratamento para opções inválidas no menu

--- gerador_de_senha_lista.py
+++ gerador_de_senha_lista.py
@@ -4,6 +4,13 @@ import random
 keys = []
 caracters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&*()_+"
 
+#menu
+print("Escolha uma opção:")
+print("1 - Gerar senha")
+print("2 - Listar senhas")
+print("3 - Remover senha")
+print("4 - Sair")
+
 
 def gerar_senha(lista):
   tamanho =  int(input("digite a quantidade de caracteres que deseja na senha: "))
@@ -17,13 +24,56 @@ def gerar_senha(lista):
   time.sleep(1)
       
 def listar_senhas(lista):
-    if not keys:
-        print("nenhuma senha encontrada.")
+    if not lista:
+        print("Nenhuma Senha encontrada.")
     else:
         print("\nSenhas:")
-        for i, t in enumerate(keys,0):
-            print(f"{i+1}. {t}")
-        print("Fim da lista de senhas.\n")
-        time.sleep(1)
-        print("senhas listadas com sucesso!\n")
-
+    for i, t in enumerate(lista,0):
+        print(f"{i+1}. {t}")
+    print("Fim da lista de senhas.\n") 
+    
+def remover_senha(lista):
+    print("Digite o número da senha a remover: ", end="")
+    if not lista:
+        print("Nenhuma senha encontrada.")
+        return
+    listar_senhas(lista)
+    try:
+        print("Listando senhas salvas\n ", end="")
+        index = int(input()) - 1
+        if 0 <= index < len(lista):
+            senha_removida = lista.pop(index)
+            print(f"Senha '{senha_removida}' removida com sucesso!\n")
+        else:
+            print("Número inválido.")
+    except ValueError:
+        print("Entrada inválida. Por favor, insira um número.")
+        
+def executar_programa():
+        while True:
+            opcao = input("Escolha uma opção (1-4): ")
+            if opcao == "1":
+                gerar_senha(keys)
+            elif opcao == "2":
+                listar_senhas(keys)
+            elif opcao == "3":
+                remover_senha(keys)
+            elif opcao == "4":
+                print("finalizando aplicações ...")
+                time.sleep(3)
+                print("Encerrando keys)...")
+                time.sleep(2.5)
+                print("Encerrando o sistema de keys)...")
+                time.sleep(2)
+                print("desligando as luzes...")
+                time.sleep(1.5)
+                print("desligando o ar condicionado...")
+                time.sleep(1)
+                print("desligando o computador...")
+                time.sleep(0.5)
+                print("Encerrando o sistema... Até logo, Dev H-Byte!")
+                break
+            else:
+                print("Opção inválida. Tente novamente.")
+                
+executar_programa()
